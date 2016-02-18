21dad617e7a112a86d0597a9c70bdce0b5eb4ec4
nancyjjn <nancyjjn@gmail.com>
2016-02-18 23:22:56 +0000

    ana lab

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
-
+ 
 exports.view = function(req, res){
+	var random = Math.random();
+	console.log(random);
+	if (random >= 0.0) {
+		projects["grid"] = false;
+		res.render('index', projects);
+	}
+	else {
+		res.redirect('/grid');
+	}
+  
+};
+
+exports.viewGrid = function(req, res){
+	projects["grid"] = true;
   	res.render('index', projects);
 };
